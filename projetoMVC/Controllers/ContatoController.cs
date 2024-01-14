b87008fd64a7b906f70279a89472165f92f2986c
Andressa Abrantes <andressasabrantes@gmail.com>
2024-01-14 06:35:57 -0300

    Exibindo a lista de contatos na view e implementando no ContatoController

--- projetoMVC/Controllers/ContatoController.cs
+++ projetoMVC/Controllers/ContatoController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using projetoMVC.Context;
 
 namespace projetoMVC.Controllers
 {
     public class ContatoController : Controller
     {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult Index() 
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
